7ccbb174c3e565cb6ce272d02da3ba259d4a7dfa
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-01-21 17:05:56 +0200

    Updated.

--- src/bmi.js
+++ src/bmi.js
@@ -1,6 +1,5 @@
 import Atom      from "bacon.atom"
 import B, {bind} from "bacon.react.html"
-import Bacon     from "baconjs"
 import React     from "react"
 
 export const model = () => {
@@ -8,8 +7,7 @@ export const model = () => {
   const height = Atom(170)
   return {weight,
           height,
-          bmi: Bacon.combineWith(weight, height, (w, h) =>
-                                 Math.round(w/(h * h * 0.0001)))}
+          bmi: B(weight, height, (w, h) => Math.round(w/(h * h * 0.0001)))}
 }
 
 const Slider = ({title, units, value, ...props}) => <div>
@@ -18,7 +16,7 @@ const Slider = ({title, units, value, ...props}) => <div>
 </div>
 
 export const Control = ({model: {weight, height, bmi}}) => <div>
-  <Slider title="Weight" units="kg" min={40} max={140} value={weight}/>
+  <Slider title="Weight" units="kg" min={40}  max={140} value={weight}/>
   <Slider title="Height" units="cm" min={140} max={210} value={height}/>
   <B.div>BMI: {bmi}</B.div>
 </div>
