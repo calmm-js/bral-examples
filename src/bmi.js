26aca447b30e3c5106a2c590dfb79d208eca49f4
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-01-24 20:17:29 +0200

    CSS for BMI

--- src/bmi.js
+++ src/bmi.js
@@ -1,24 +1,32 @@
-import Atom      from "bacon.atom"
-import B, {bind} from "bacon.react.html"
-import React     from "react"
+import Atom               from "bacon.atom"
+import B, {bind, classes} from "bacon.react.html"
+import React              from "react"
 
-const Slider = ({title, units, value, ...props}) => <div>
-  <B.div>{title}: {value}{units}</B.div>
-  <B.input type="range" {...bind({value})} {...props}/>
-</div>
+const Slider = ({title, units, value, ...props}) =>
+  <div>
+    <B.div>{title}: {value}{units}</B.div>
+    <B.input type="range" {...bind({value})} {...props}/>
+  </div>
 
-const Control = ({model: {weight, height, bmi}}) => <div>
-  <Slider title="Weight" units="kg" min={40}  max={140} value={weight}/>
-  <Slider title="Height" units="cm" min={140} max={210} value={height}/>
-  <B.div>BMI: {bmi}</B.div>
-</div>
+const Control = ({model: {weight, height, bmi, classification}}) =>
+  <B.div {...classes("bmi", classification)}>
+    <Slider title="Weight" units="kg" min={40}  max={140} value={weight}/>
+    <Slider title="Height" units="cm" min={140} max={210} value={height}/>
+    <B.div>BMI: <B.span className="bmi-value">{bmi}</B.span></B.div>
+  </B.div>
 
-Control.model = () => {
-  const weight = Atom(70)
-  const height = Atom(170)
-  return {weight,
-          height,
-          bmi: B(weight, height, (w, h) => Math.round(w/(h * h * 0.0001)))}
+Control.model = ({weight = Atom(80), height = Atom(180)} = {}) => {
+  const bmi = B(weight, height, (w, h) => Math.round(w/(h * h * 0.0001)))
+  const classification =
+    B(bmi, bmi =>
+        bmi < 15   ? "bmi-underweight bmi-underweight-severely"
+      : bmi < 18.5 ? "bmi-underweight"
+      : bmi < 25   ? "bmi-normal"
+      : bmi < 30   ? "bmi-overweight"
+      : bmi < 35   ? "bmi-obese"
+      : bmi < 40   ? "bmi-obese bmi-obese-severely"
+      :              "bmi-obese bmi-obese-very")
+  return {weight, height, bmi, classification}
 }
 
 export default Control
