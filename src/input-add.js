11912d027cf487bfeef95b78f8cd0929aa2615aa
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-02-22 13:40:37 +0200

    Don't add empty elems.

--- src/input-add.js
+++ src/input-add.js
@@ -8,8 +8,11 @@ export default ({elems = Atom([]), entry = Atom("")}) =>
     <div>
       <B.input type="text" {...bind({value: entry})}/>
       <button onClick={() => {
-        elems.modify(R.append(entry.get()))
-        entry.set("")
+        const elem = entry.get().trim()
+        if (elem) {
+          elems.modify(R.append(elem))
+          entry.set("")
+        }
       }}>Add</button>
     </div>
     <B.ul>
