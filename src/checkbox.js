1b30e145b6a2dfdab6145d53ca130073652cfbee
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-02-22 18:40:42 +0200

    Simplified

--- src/checkbox.js
+++ src/checkbox.js
@@ -2,10 +2,8 @@ import Atom      from "bacon.atom"
 import B, {bind} from "bacon.react.html"
 import React     from "react"
 
-export default () => {
-  const checked = Atom(false)
-  return <div>
-      <label><B.input type="checkbox" {...bind({checked})}/>Toggle me</label>
-      <B.p>{B(checked, c => c ? "ON" : "off")}</B.p>
-    </div>
-}
+export default ({checked = Atom(false)}) =>
+  <div>
+    <label><B.input type="checkbox" {...bind({checked})}/>Toggle me</label>
+    <B.p>{B(checked, c => c ? "ON" : "off")}</B.p>
+  </div>
